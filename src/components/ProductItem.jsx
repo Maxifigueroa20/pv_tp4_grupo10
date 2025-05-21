27fe0b51eda6edb183eb6e88a1b1ab9be1612608
Maxy <marianof0101@gmail.com>
2025-05-21 20:23:50 -0300

    feat: se desarrolló la lógica de los componentes ProductList y ProductItem [MaximilianoFigueroa]

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,30 @@
-const ProductItem = () => {
+const ProductItem = ({ producto, eliminarProducto, setProductoEditando }) => {
     return (
-        <>
-        </>
+        <li className="producto">
+        <div className="detalle">
+            <p className="nombre">{producto.descripcion}</p>
+            <p>ID: {producto.id}</p>
+            <p>
+                Precio: ${producto.precioUnitario} | Descuento: {producto.descuento}%
+                | Final: ${producto.precioConDescuento.toFixed(2)}
+            </p>
+            <p>Stock: {producto.stock}</p>
+        </div>
+        <div className="acciones">
+            <button
+                onClick={() => setProductoEditando(producto)}
+                className="boton chico editar"
+            >
+                Editar
+            </button>
+            <button
+                onClick={() => eliminarProducto(producto.id)}
+                className="boton chico eliminar"
+            >
+                Eliminar
+            </button>
+        </div>
+      </li>
     )
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,19 @@
-const ProductList = () => {
+import ProductItem from "./ProductItem"
+
+const ProductList = ({ productos, eliminarProducto, setProductoEditando }) => {
+    if (productos.length === 0) return <p>No hay productos para mostrar.</p>
+
     return (
-        <>
-        </>
+    <ul className="lista">
+        {productos.map((p) => (
+            <ProductItem
+            key={p.id}
+            producto={p}
+            eliminarProducto={eliminarProducto}
+            setProductoEditando={setProductoEditando}
+            />
+        ))}
+    </ul>
     )
 }
 
